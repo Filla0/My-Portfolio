7d8f1bff9e43a20ea2cdb5b9150e097f142ed8d1
Filimon Haftu <filimonhaftu18@gmail.com>
2023-07-09 16:38:21 +0200

    contact page done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import "./contact.css"
 
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_93fscu9', 'template_0a6zihh', form.current, 'LgUqXvf4vp5ZTwPWb')
+        e.target.reset()
+    };
     return (
         <div className="contact section" id="contact">
             <h2 className="section__title">Get in touch</h2>
@@ -50,7 +60,7 @@ const Contact = () => {
                 <div className="contact__content">
                     <h3 className="contact__title">Write me your project</h3>
 
-                    <form action="" className="contact__form">
+                    <form ref={form} onSubmit={sendEmail} action="" className="contact__form">
                         <div className="contact__form-div">
                             <label className="contact__form-tag">Name</label>
                             <input type="text" name="name" className="contact__form-input" placeholder="Insert your name" />
@@ -61,7 +71,7 @@ const Contact = () => {
                             <input type="email" name="emai" className="contact__form-input" placeholder="Insert your email" />
                         </div>
 
-                        <div className="contact__form-div">
+                        <div className="contact__form-div contact__form-area">
                             <label className="contact__form-tag">Project</label>
                             <textarea name="project" cols="30" rows="10" className="contact__form-input" placeholder="Write your project"></textarea>
                         </div>
